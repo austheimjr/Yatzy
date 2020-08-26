c53e356bfdda3ea2c1842ff546d8c88f9f841613
Marius Austheim <56087455+austheimjr@users.noreply.github.com>
2020-08-26 14:28:35 +0200

    hhh

---
+++ src/main/java/no/kristiania/yatzy/YatzyGame.java
@@ -0,0 +1,7 @@
+package no.kristiania.yatzy;
+
+public class YatzyGame {
+    public Object score(YatzyCategory ones, int[] ints) {
+        return 0;
+    }
+}
---
+++ src/test/java/no/kristiania/yatzy/YatzyTest.java
@@ -0,0 +1,16 @@
+package no.kristiania.yatzy;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class YatzyTest{
+
+    @Test
+    void ShouldScoreOnes(){
+        assertEquals(4, new YatzyGame().score(YatzyCategory.ONES, new int[] {1, 1, 2, 1, 1}));
+    }
+
+
+}
