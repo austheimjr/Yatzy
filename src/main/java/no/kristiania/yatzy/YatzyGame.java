2426446ea90d8324759e9229c76171f03dd41b1c
Marius Austheim <56087455+austheimjr@users.noreply.github.com>
2020-08-27 22:34:32 +0200

    ONES green, TWOS red

--- src/main/java/no/kristiania/yatzy/YatzyGame.java
+++ src/main/java/no/kristiania/yatzy/YatzyGame.java
@@ -3,10 +3,12 @@ package no.kristiania.yatzy;
 public class YatzyGame {
 
     public Object score(YatzyCategory category, int[] dice) {
-        int result = 4;
+        int result = 0;
         for (int die : dice){
-            if(die == 1){
-                result += 1;
+            if(die == 1 && category == YatzyCategory.ONES){
+                result += die;
+            }else if (die == 2 && category == YatzyCategory.TWOS){
+                result += die;
             }
         }
         return result;
