3b2b083dc9caf01b895d0e1e275d1366ff816a60
Marius Austheim <56087455+austheimjr@users.noreply.github.com>
2020-08-29 18:08:33 +0200

    Implement PAIR and THREE_OF_A_KIND (#1)
    * red test for PAIR
    
    * PAIRs implemented, new red test
    
    * red test for PAIR
    
    * PAIR green, added THREE OF A KIND, also GREEN

--- src/test/java/no/kristiania/yatzy/YatzyTest.java
+++ src/test/java/no/kristiania/yatzy/YatzyTest.java
@@ -10,15 +10,26 @@ public class YatzyTest{
     private final YatzyGame yatzyGame = new YatzyGame();
 
     @Test
-    void ShouldScoreOnes(){
+    void shouldScoreOnes(){
         assertEquals(4, yatzyGame.score(YatzyCategory.ONES, new int[] {1, 1, 2, 1, 1}));
         assertEquals(2, yatzyGame.score(YatzyCategory.ONES, new int[] {6, 5, 2, 1, 1}));
     }
 
     @Test
-    void ShouldScoreTwo(){
+    void shouldScoreTwo(){
         assertEquals(6, yatzyGame.score(YatzyCategory.TWOS, new int[] {1, 2, 2, 2, 1}));
         assertEquals(4, yatzyGame.score(YatzyCategory.TWOS, new int[] {6, 2, 2, 1, 1}));
+    }
+    @Test
+    void shouldScorePair(){
+        assertEquals(10, yatzyGame.score(YatzyCategory.PAIR, new int[] {1, 3, 5, 5, 2}));
+        assertEquals(12, yatzyGame.score(YatzyCategory.PAIR, new int[] {1, 6, 6, 5, 2}));
+        assertEquals(12, yatzyGame.score(YatzyCategory.PAIR, new int[] {5, 5, 6, 6, 2}));
+    }
+    @Test
+    void shouldScoreThreeOfAKind(){
+        assertEquals(12, yatzyGame.score(YatzyCategory.THREE_OF_A_KIND, new int[]{4, 4, 6, 4, 2}));
+        assertEquals(18, yatzyGame.score(YatzyCategory.THREE_OF_A_KIND, new int[]{4, 6, 6, 4, 6}));
     }
 
 
